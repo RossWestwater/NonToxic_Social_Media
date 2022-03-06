605604448c236426bf685b6d5621ae6b0e75c81e
Ross Westwater <ross.westwater@gmail.com>
2022-03-06 12:09:26 -0700

    added thought and user controllers. Most routes working but not delete friend. haven't tested the reaction routes

---
+++ controllers/thought-controller.js
@@ -0,0 +1,73 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+  // get all Thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .populate({
+        path: 'reactions'
+      })
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one Thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: 'reactions',
+      })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // createThought
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  },
+
+  // update Thought by id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No Thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // delete Thought
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  },
+
+  addReaction({ params }, res) {
+    Thought.findByIdAndUpdate({ _id: params.thoughtId }, {$addToSet: {reactions: params.friendId } }, {new: true, runValidators: true })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  },
+
+  deleteReaction({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId }, {$pull: {reactions: params.friendId } }, { new: true} )
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  }
+
+};
+
+
+module.exports = thoughtController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,46 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280 
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
 
 const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
       required: true,
-      minlength: 1,
-      maxlength: 280
+      minLength: 1,
+      maxLength: 280
     },
     createdAt: {
       type: Date,
-      default: new Date.now,
+      default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     },
     username: {
@@ -33,36 +63,6 @@ const thoughtSchema = new Schema(
 thoughtSchema.virtual('reactionCount').get(function() {
   return this.reactions.length
 });
-
-const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280 
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    createdAt: {
-      type: Date,
-      default: new Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    }
-  },
-  {
-    toJSON: {
-      getters: true
-    },
-    // prevents virtuals from creating duplicate of _id as `id`
-    id: false
-  }
-);
 
 
 const Thought = model('Thought', thoughtSchema);
